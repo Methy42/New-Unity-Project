b110395561e9485e6b1b79434bad1ab12aa2b0b9
tanghuayu <tanghuayu@archeros.com>
2024-02-16 19:03:44 +0800

    [feat]: 提交场景文件和Effect的类文件

---
+++ Assets/EffectEnity.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class EffectEnity
+{
+    public int id;
+    public string name;
+    public string type;
+    public GameObject effectGameObject;
+}
